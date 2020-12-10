94ec7b39e80b78828563fea65cbbedf1d5d571ee
a11ce <a11ce@protonmail.com>
2020-12-10 02:39:24 -0500

    YAPF

--- vocabulary.py
+++ vocabulary.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
+
 def newVocabulary(newMorphemeF):
     vocab = {}
-    with open(Path(__file__).parent/'words.ls') as f:
+    with open(Path(__file__).parent / 'words.ls') as f:
         curCat = ""
         for line in f:
             if line[0] == ";":
